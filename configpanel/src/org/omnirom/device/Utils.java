600d9e91965ee45940b303b3538e1fe5c07eaf00
enzoo96 <enzoo.site@gmail.com>
2018-04-09 12:14:42 +0000

    WIP msm8996-common: configpanel: Add doze mode
    Change-Id: Ie9f8af760f9f6cf5292aaf5dbe427a5d9578b31d
    Signed-off-by: enzoo96 <enzoo.site@gmail.com>

--- configpanel/src/org/omnirom/device/Utils.java
+++ configpanel/src/org/omnirom/device/Utils.java
@@ -18,6 +18,7 @@ package org.omnirom.device;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -26,10 +27,92 @@ import android.content.SharedPreferences;
 import android.os.UserHandle;
 import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.PreferenceManager;
+import android.provider.Settings;
+import android.util.Log;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
+import static android.provider.Settings.Secure.DOZE_ENABLED;
+
 public class Utils {
+
+    private static final String TAG = "DozeUtils";
+    private static final boolean DEBUG = false;
+
+    private static final String DOZE_INTENT = "com.android.systemui.doze.pulse";
+
+    protected static final String CATEG_PROX_SENSOR = "proximity_sensor";
+
+    protected static final String GESTURE_PICK_UP_KEY = "gesture_pick_up";
+    protected static final String GESTURE_HAND_WAVE_KEY = "gesture_hand_wave";
+    protected static final String GESTURE_POCKET_KEY = "gesture_pocket";
+
+    protected static void startService(Context context) {
+        if (DEBUG) Log.d(TAG, "Starting service");
+        context.startServiceAsUser(new Intent(context, DozeService.class),
+                UserHandle.CURRENT);
+    }
+
+    protected static void stopService(Context context) {
+        if (DEBUG) Log.d(TAG, "Stopping service");
+        context.stopServiceAsUser(new Intent(context, DozeService.class),
+                UserHandle.CURRENT);
+    }
+
+    protected static void checkDozeService(Context context) {
+        if (isDozeEnabled(context) && sensorsEnabled(context)) {
+            startService(context);
+        } else {
+            stopService(context);
+        }
+    }
+
+    protected static boolean getProxCheckBeforePulse(Context context) {
+        try {
+            Context con = context.createPackageContext("com.android.systemui", 0);
+            int id = con.getResources().getIdentifier("doze_proximity_check_before_pulse",
+                    "bool", "com.android.systemui");
+            return con.getResources().getBoolean(id);
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
+
+    protected static boolean isDozeEnabled(Context context) {
+        return Settings.Secure.getInt(context.getContentResolver(),
+                DOZE_ENABLED, 1) != 0;
+    }
+
+    protected static boolean enableDoze(boolean enable, Context context) {
+        return Settings.Secure.putInt(context.getContentResolver(),
+                DOZE_ENABLED, enable ? 1 : 0);
+    }
+
+    protected static void launchDozePulse(Context context) {
+        if (DEBUG) Log.d(TAG, "Launch doze pulse");
+        context.sendBroadcastAsUser(new Intent(DOZE_INTENT),
+                new UserHandle(UserHandle.USER_CURRENT));
+    }
+
+    protected static boolean pickUpEnabled(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getBoolean(GESTURE_PICK_UP_KEY, false);
+    }
+
+    protected static boolean handwaveGestureEnabled(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getBoolean(GESTURE_HAND_WAVE_KEY, false);
+    }
+
+    protected static boolean pocketGestureEnabled(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getBoolean(GESTURE_POCKET_KEY, false);
+    }
+
+    protected static boolean sensorsEnabled(Context context) {
+        return pickUpEnabled(context) || handwaveGestureEnabled(context)
+                || pocketGestureEnabled(context);
+    }
 
     /**
      * Write a string value to the specified file.
