c3c0c0ab88a71e81b35358ad25183488e241a035
enzoo96 <enzoo.site@gmail.com>
2018-04-13 15:20:04 +0000

    msm8996-common: rename ConfigPanel to XiaomiParts
    Change-Id: I267383da94e53f3ebe6fe1b431ac65db600b7e66
    Signed-off-by: enzoo96 <enzoo.site@gmail.com>

--- XiaomiParts/Android.mk
+++ XiaomiParts/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
-LOCAL_PACKAGE_NAME := ConfigPanel
+LOCAL_PACKAGE_NAME := XiaomiParts
 LOCAL_REQUIRED_MODULES := XiaomiPocketMode
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
