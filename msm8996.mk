0370e1eb22ea56ff5e09ed1d3da337f1f33b7641
enzoo96 <enzoo.site@gmail.com>
2018-09-03 14:55:31 +0000

    Revert "msm8996-common: Build QTI HIDL power 1.0 HAL"
    This reverts commit e850cd2e562462f38948ace3060dd825a8cd0db7.

--- msm8996.mk
+++ msm8996.mk
@@ -293,7 +293,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service-qti
+    android.hardware.power@1.0-impl \
+    power.msm8996
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
