c3ae2b5efbecc5dcfbc5513dd1856b518b68887c
enzoo96 <enzoo.site@gmail.com>
2018-12-28 15:23:52 +0100

    msm8996-common: add persist.radio.aosp_usr_pref_sel

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -146,7 +146,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.redir_party_num=1 \
     persist.vendor.radio.custom_ecc=1 \
     persist.vendor.radio.rat_on=combine \
-    persist.vendor.radio.sib16_support=1
+    persist.vendor.radio.sib16_support=1 \
+    persist.radio.aosp_usr_pref_sel=true
 
 # RmNet Data
 PRODUCT_PROPERTY_OVERRIDES += \
