55bd44e6a2a23efe665f9fcb06fc9ec1e10edc57
enzoo96 <enzoo.site@gmail.com>
2019-01-03 09:11:23 +0100

    Revert "msm8996-common: Switch to common basic USB HAL"
    This reverts commit 334ea750d06ad6370333f24af0295b4b53e03c7a.

--- msm8996.mk
+++ msm8996.mk
@@ -349,10 +349,6 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
-# USB
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
-
 # VNDK
 PRODUCT_PACKAGES += \
     vndk-sp
